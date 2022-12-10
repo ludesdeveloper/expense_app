738cb40924c3d6accc49f98569d4a93af3e0f4a1
seskiramadhan <seski.ramadhan@gmail.com>
2022-12-10 22:01:50 +0700

    adding source

--- dynamodb.py
+++ dynamodb.py
@@ -4,10 +4,10 @@ import boto3
 EXPENSE_TABLE = os.environ['EXPENSE_TABLE']
 
 
-def insert_expense(expense_id, total):
+def insert_expense(expense_id, total, source):
     dynamodb_client = boto3.client('dynamodb')
 
     dynamodb_client.put_item(
         TableName=EXPENSE_TABLE, Item={
-            'expenseId': {'S': expense_id}, 'total': {'S': total}}
+            'expenseId': {'S': expense_id}, 'total': {'S': total}, 'source': {'S': source}}
     )
