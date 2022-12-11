133336ab3d7070c058537a136b3250480873612e
seskiramadhan <seski.ramadhan@gmail.com>
2022-12-11 21:14:41 +0700

    check username

--- app.py
+++ app.py
@@ -20,6 +20,9 @@ def index():
     if request.method == 'POST':
         msg = request.get_json()
         try:
+            print('debug username')
+            print(msg['message']['chat']['username'])
+            print('debug username')
             file_id, chat_id, update_id, caption, date = tel_parse_get_message(
                 msg)
             if file_id != 'notfound':
